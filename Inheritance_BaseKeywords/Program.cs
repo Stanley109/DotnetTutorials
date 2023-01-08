7c3eabfeef38bd15534922c144f49e28965b6514
jason <jasonstanleykho@gmail.com>
2023-01-08 18:42:45 +0800

    updated README file

--- Inheritance_BaseKeywords/Program.cs
+++ Inheritance_BaseKeywords/Program.cs
@@ -4,19 +4,28 @@ using System;
 class Program {         
         static void Main(string[] args)
         {
-            Vehicle v = new Car("medium car size");
-            v.displaySize(); // displays Car size
-            v = new Vehicle("Standard car size");
+            Vehicle v = new Vehicle("Standard vehicle size");
             v.displaySize(); // displays Vehicle size
-            //v.callBaseDisplaySize()  //this won't work because vehicle does not contain 'callBaseDisplaySize' method.
-            Car c = new Car("super car size");
+            Vehicle w = new Car("Medium car size",100); // is this possible that the child constructor can have more or parameters than its parent class
+            w.displaySize(); // displays Car size 
+            //Console.WriteLine(w.cost);     //this won't work because vehicle does not contain the property 'cost.
+            //w.callBaseDisplaySize();  //this won't work because vehicle does not contain the 'callBaseDisplaySize' method.
+            Car c = new Car("Super car size",500);  //
             c.callBaseDisplaySize();    //this will be able to call callBaseDisplaySize because we instantiated its own class type
+            c.displayCost();
+
+            Vehicle[] vehicles = {v,w};
+            foreach(Vehicle vehicle in vehicles)
+            {
+                vehicle.displaySize();
+            }
+
         }
 }
 
 class Vehicle {
 
-    public string vehicleSize {get;set;}
+    protected string vehicleSize {get;set;}
     public Vehicle(string vehicleSize)
     {
         this.vehicleSize = vehicleSize;
@@ -29,9 +38,10 @@ class Vehicle {
 }
 
 class Car:Vehicle {
-    public Car(string carSize):base(carSize)    //by using :base(carSize) The car Constructor  will call the superclass's constructor passing its paramater carSize 
+    public int cost {get;set;}
+    public Car(string carSize, int cost):base(carSize)    //by using :base(carSize) The car Constructor  will call the superclass's constructor passing its paramater carSize 
     {
-
+        this.cost = cost;
     }  
     
     public override void displaySize()  //override keyword is need to override the same method name it parent's class has1
@@ -41,5 +51,9 @@ class Car:Vehicle {
     public void callBaseDisplaySize()
     {
         base.displaySize();   //this will call the Car's parent class makeSound which will displaye "Vehicle sound"
+    }
+    public void displayCost()
+    {
+        Console.WriteLine("Car cost is: " + cost);
     }
 }
